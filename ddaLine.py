406f22c0bc04e83313a1a7bb7ee2ffc26762ca63
NithinYesudas <nithinypadickal@gmail.com>
2023-01-12 00:02:31 +0530

    made some changes

--- ddaLine.py
+++ ddaLine.py
@@ -8,25 +8,28 @@ def clearScreen():
 
 def dda(x1,y1,x2,y2):
     
-    m = (y2-y1)/(x2-x1)
+    dy =y2-y1
+    dx =x2-x1
     glPointSize(10)
+    steps = 0
     for i in range((y2-y1)):
-        glBegin(GL_POINTS)
-        glVertex2f(x1,y1)
-        glEnd()
-        glFlush()
-        if(m<0):
+        if(dx>dy):
             x1 += 1
             y1 += int(m)
         else:
             x1 += int(1/m)
-            y1 += 1    
+            y1 += 1 
+        glBegin(GL_POINTS)
+        glVertex2f(x1,y1)
+        glEnd()
+        glFlush()
+           
     
 def main():
     x1 = int(input("Enter x1: "))
-    y1 = int(input("Enter y2: "))
+    y1 = int(input("Enter y1: "))
     x2 = int(input("x2: "))
-    y2 = int(input("x2: "))
+    y2 = int(input("y2: "))
     glutInit()
     glutInitWindowSize(500,500)
     glutCreateWindow("DDA Line") 
